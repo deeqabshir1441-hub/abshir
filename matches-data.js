71448eaa81863f8086344addf0a42ebe37f40c87
deeq <deeqabshir1441@gmail.com>
2026-07-15 15:21:26 +0300

    First upload

--- matches-data.js
+++ matches-data.js
@@ -1,26 +1,22 @@
 const matchesData = {
     shalay: [
-        // Yesterday matches
-    ],
-
-    maanta: [
-{
+    {
     home: 'France',
     away: 'Spain',
     homeScore: 0,
-    awayScore: 0,
+    awayScore: 2,
     league: 'FIFA World Cup 2026',
-    status: 'Upcoming',
-    statusClass: 'status-upcoming',
+    status: 'Finished',
+    statusClass: 'status-finished',
     overlayText: 'Watch Live',
     url: 'watch-live2.html',
     displayTime: '22:00',
     matchDate: '2026-07-14',
     id: '2026095'
-}
+    }   
     ],
 
-    berri: [
+    maanta: [
    {
     home: 'England',
     away: 'Argentina',
@@ -34,6 +30,10 @@ const matchesData = {
     displayTime: '22:00',
     matchDate: '2026-07-15',
     id: '2026096'
-}
+    }
+    ],
+
+    berri: [
+
     ]
 };
