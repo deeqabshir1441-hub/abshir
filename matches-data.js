cbff0590f8080be5b67474e4724cb0276b02e28d
deeq <deeqabshir1441@gmail.com>
2026-08-02 13:36:49 +0300

    First upload

--- matches-data.js
+++ matches-data.js
@@ -1,30 +1,13 @@
 const matchesData = {
     shalay: [
-    {
-    home: 'France',
-    away: 'Spain',
-    homeScore: 0,
-    awayScore: 2,
-    league: 'FIFA World Cup 2026',
-    status: 'Finished',
-    statusClass: 'status-finished',
-    overlayText: 'Watch Live',
-    url: 'watch-live2.html',
-    displayTime: '22:00',
-    matchDate: '2026-07-14',
-    id: '2026095'
-    }   
-    ],
-
-    maanta: [
 {
     home: 'Manchester United',
     away: 'Atlético Madrid',
     homeScore: 2,
     awayScore: 1,
     league: 'Club Friendly',
-    status: 'Upcoming',
-    statusClass: 'status-upcoming',
+    status: 'Finished',
+    statusClass: 'status-finished',
     overlayText: 'Watch Live',
     url: 'watch-live.html',
     displayTime: '16:00',
@@ -34,11 +17,11 @@ const matchesData = {
 {
     home: 'Real Madrid',
     away: 'Fiorentina',
-    homeScore: 0,
-    awayScore: 0,
+    homeScore: 2,
+    awayScore: 2,
     league: 'Club Friendly',
-    status: 'Upcoming',
-    statusClass: 'status-upcoming',
+    status: 'Finished',
+    statusClass: 'status-finished',
     overlayText: 'Watch Live',
     url: 'watch-live1.html',
     displayTime: '19:00',
@@ -48,11 +31,11 @@ const matchesData = {
 {
     home: 'Girona',
     away: 'Arsenal',
-    homeScore: 0,
-    awayScore: 0,
+    homeScore: 1,
+    awayScore: 4,
     league: 'Club Friendly',
-    status: 'Upcoming',
-    statusClass: 'status-upcoming',
+    status: 'Finished',
+    statusClass: 'status-finished',
     overlayText: 'Watch Live',
     url: 'watch-live2.html',
     displayTime: '21:00',
@@ -65,13 +48,30 @@ const matchesData = {
     homeScore: 1,
     awayScore: 1,
     league: 'Club Friendly',
-    status: 'Upcoming',
-    statusClass: 'status-upcoming',
+    status: 'Finished',
+    statusClass: 'status-finished',
     overlayText: 'Watch Live',
     url: 'watch-live3.html',
     displayTime: '14:30',
     matchDate: '2026-08-01',
     id: '2026102'
+} 
+    ],
+
+    maanta: [
+{
+    home: 'Liverpool',
+    away: 'Leeds United',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'Club Friendly',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live.html',
+    displayTime: '23:00',
+    matchDate: '2026-08-02',
+    id: '2026103'
 }
     ],
 
