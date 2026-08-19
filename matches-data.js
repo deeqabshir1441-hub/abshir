c6ea8cf278099d3932e2d328631cb6a369d2f62d
deeq <deeqabshir1441@gmail.com>
2026-08-19 16:44:50 +0300

    First upload

--- matches-data.js
+++ matches-data.js
@@ -1,67 +1,26 @@
 const matchesData = {
     shalay: [
-{
-    home: 'Manchester United',
-    away: 'Atlético Madrid',
-    homeScore: 2,
-    awayScore: 1,
-    league: 'Club Friendly',
-    status: 'Finished',
-    statusClass: 'status-finished',
-    overlayText: 'Watch Live',
-    url: 'watch-live.html',
-    displayTime: '16:00',
-    matchDate: '2026-08-01',
-    id: '2026099'
-},
-{
-    home: 'Real Madrid',
-    away: 'Fiorentina',
-    homeScore: 2,
-    awayScore: 2,
-    league: 'Club Friendly',
-    status: 'Finished',
-    statusClass: 'status-finished',
-    overlayText: 'Watch Live',
-    url: 'watch-live1.html',
-    displayTime: '19:00',
-    matchDate: '2026-08-01',
-    id: '2026100'
-},
-{
-    home: 'Girona',
-    away: 'Arsenal',
-    homeScore: 1,
-    awayScore: 4,
-    league: 'Club Friendly',
-    status: 'Finished',
-    statusClass: 'status-finished',
-    overlayText: 'Watch Live',
-    url: 'watch-live2.html',
-    displayTime: '21:00',
-    matchDate: '2026-08-01',
-    id: '2026101'
-},
-{
-    home: 'Manchester City',
-    away: 'Inter Milan',
-    homeScore: 1,
-    awayScore: 1,
-    league: 'Club Friendly',
-    status: 'Finished',
-    statusClass: 'status-finished',
-    overlayText: 'Watch Live',
-    url: 'watch-live3.html',
-    displayTime: '14:30',
-    matchDate: '2026-08-01',
-    id: '2026102'
-} 
+ 
     ],
 
     maanta: [
 {
-    home: 'Liverpool',
-    away: 'Leeds United',
+    home: 'Barcelona',
+    away: 'Al Ahly SC',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'Club Friendly',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live1.html',
+    displayTime: '21:00',
+    matchDate: '2026-08-19',
+    id: '2026105'
+},
+{
+    home: 'Atletico Madrid',
+    away: 'Malaga',
     homeScore: 0,
     awayScore: 0,
     league: 'Club Friendly',
@@ -69,9 +28,9 @@ const matchesData = {
     statusClass: 'status-upcoming',
     overlayText: 'Watch Live',
     url: 'watch-live.html',
-    displayTime: '23:00',
-    matchDate: '2026-08-02',
-    id: '2026103'
+    displayTime: '22:00',
+    matchDate: '2026-08-19',
+    id: '2026104'
 }
     ],
 
