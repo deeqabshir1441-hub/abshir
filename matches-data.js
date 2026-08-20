f91de489cc3203c226cc1721e8b43ad78651af62
deeq <deeqabshir1441@gmail.com>
2026-08-21 02:22:19 +0300

    First upload

--- matches-data.js
+++ matches-data.js
@@ -1,17 +1,13 @@
 const matchesData = {
     shalay: [
- 
-    ],
-
-    maanta: [
-{
+ {
     home: 'Rayo Vallecano',
     away: 'Deportivo Alavés',
-    homeScore: 0,
-    awayScore: 0,
+    homeScore: 1,
+    awayScore: 1,
     league: 'La Liga',
-    status: 'Upcoming',
-    statusClass: 'status-upcoming',
+    status: 'Finished',
+    statusClass: 'status-finished',
     overlayText: 'Watch Live',
     url: 'watch-live2.html',
     displayTime: '22:00',
@@ -20,7 +16,121 @@ const matchesData = {
 }
     ],
 
-    berri: [
+    maanta: [
+{
+    home: 'Arsenal',
+    away: 'Coventry City',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'Premier League',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live3.html',
+    displayTime: '22:00',
+    matchDate: '2026-08-21',
+    id: '2026107'
+}
+    ],
 
+    berri: [
+{
+    home: 'Hull City',
+    away: 'Manchester United',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'Premier League',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live.html',
+    displayTime: '14:30',
+    matchDate: '2026-08-22',
+    id: '2026108'
+},
+{
+    home: 'Everton',
+    away: 'Crystal Palace',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'Premier League',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live1.html',
+    displayTime: '17:00',
+    matchDate: '2026-08-22',
+    id: '2026109'
+},
+{
+    home: 'Ipswich Town',
+    away: 'Sunderland',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'Premier League',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live2.html',
+    displayTime: '17:00',
+    matchDate: '2026-08-22',
+    id: '2026110'
+},
+{
+    home: 'Nottingham Forest',
+    away: 'Leeds United',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'Premier League',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live3.html',
+    displayTime: '17:00',
+    matchDate: '2026-08-22',
+    id: '2026111'
+},
+{
+    home: 'Brentford',
+    away: 'Tottenham Hotspur',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'Premier League',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live.html',
+    displayTime: '19:30',
+    matchDate: '2026-08-22',
+    id: '2026112'
+},
+{
+    home: 'Athletic Club',
+    away: 'Sevilla',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'La Liga',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live2.html',
+    displayTime: '18:00',
+    matchDate: '2026-08-22',
+    id: '2026113'
+},
+{
+    home: 'Espanyol',
+    away: 'Real Madrid',
+    homeScore: 0,
+    awayScore: 0,
+    league: 'La Liga',
+    status: 'Upcoming',
+    statusClass: 'status-upcoming',
+    overlayText: 'Watch Live',
+    url: 'watch-live3.html',
+    displayTime: '22:30',
+    matchDate: '2026-08-22',
+    id: '2026114'
+}
     ]
 };
